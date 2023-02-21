4128da042fa7d362d650f637f372e1bc298dc8df
nuci <kirianuci@gmail.com>
2023-02-21 20:25:23 +0400

    Add search bar input component

--- store-techno/src/Page/Dashboard.jsx
+++ store-techno/src/Page/Dashboard.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import "../Style/Dashboard.css"
+import Store from './Store'
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <Navbar/>
-      <div className="store_content"></div>
+      <div className="store_content">
+        <Store/>
+      </div>
       <div className="bag"></div>
     </div>
   )
---
+++ store-techno/src/Page/Store.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Store = () => {
+  return (
+    <>
+      <div className="search_box">
+       <p className="search_title">Search Item</p>
+       <input className="search_input" type="text" placeholder="Apple Watch, Samsung S21, Macbook Pro, ..." />
+      </div>
+    </>
+  )
+}
+
+export default Store
