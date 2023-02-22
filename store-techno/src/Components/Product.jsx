920b65bb478192378971ae925c4d99019867bfc5
nuci <kirianuci@gmail.com>
2023-02-22 12:36:52 +0400

    add product container design using map method

---
+++ store-techno/src/Components/Product.jsx
@@ -0,0 +1,99 @@
+import AddToCartIcon from "../Photo/Button-add-shop.svg"
+import WatchPhoto from "../Photo/Watch.svg";
+import Headphones from "../Photo/Headphones.svg"
+import Iphone13 from "../Photo/Iphone-13.svg"
+import Iphone13Pro from "../Photo/iphone-13-Pro-02.svg"
+import iPhone12SubwayBlue from '../Photo/Iphone-12.svg'
+import iPhone11Red from "../Photo/Iphone-12red.svg"
+import iPhone12White from "../Photo/Iphone-12-white.svg"
+import Samsung21 from "../Photo/Samsung-S21.svg"
+import samsungNote21 from "../Photo/Samsung-Note21.svg"
+import Iphone11black from "../Photo/iphone-12-black-01.svg"
+
+
+export const productSectionOne = [
+ {
+  id: 1,
+  photo: WatchPhoto,
+  productName: 'Apple Watch',
+  productModel: 'Series 5 SE',
+  price: 529.99,
+  addToCartIcon: AddToCartIcon
+ },
+ {
+  id: 2,
+  photo: Headphones,
+  productName: 'Sony ZX330BT',
+  productModel: 'Light Grey',
+  price: 39.99,
+  addToCartIcon: AddToCartIcon
+ },
+ {
+  id: 3,
+  photo: Iphone13,
+  productName: 'iPhone 13',
+  productModel: 'Navy Blue',
+  price: 729.99,
+  addToCartIcon: AddToCartIcon
+ },
+ {
+  id: 4,
+  photo: Iphone13Pro,
+  productName: 'iPhone 13 Pro',
+  productModel: 'Grey',
+  price: 819.99,
+  addToCartIcon: AddToCartIcon
+ },
+ {
+  id: 5,
+  photo: iPhone12SubwayBlue,
+  productName: 'iPhone 12',
+  productModel: 'Subway Blue',
+  price: 619.99,
+  addToCartIcon: AddToCartIcon
+ },
+];
+
+export const productSectionSecond = [
+ {
+  id: 6,
+  photo: Samsung21,
+  productName: 'Samsung Galaxy S21+',
+  productModel: 'Lilac Purple',
+  price: 849.99,
+  addToCartIcon: AddToCartIcon
+ },
+
+ {
+  id: 7,
+  photo: iPhone11Red,
+  productName: 'iPhone 11 Red',
+  productModel: 'Product RED',
+  price: 3249.99,
+  addToCartIcon: AddToCartIcon
+ },
+ {
+  id: 8,
+  photo: iPhone12White,
+  productName: 'iPhone 12 White',
+  productModel: 'Milky White',
+  price: 619.99,
+  addToCartIcon: AddToCartIcon
+ },
+ {
+  id: 9,
+  photo: samsungNote21,
+  productName: "Samsung Galaxy Note",
+  productModel: '2 - Options',
+  price: 999.99,
+  addToCartIcon: AddToCartIcon
+ },
+ {
+  id: 10,
+  photo: Iphone11black,
+  productName: "Iphone 11",
+  productModel: 'Serious Black',
+  price: 696.99,
+  addToCartIcon: AddToCartIcon
+ },
+]
--- store-techno/src/Page/Store.jsx
+++ store-techno/src/Page/Store.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
+import "../Style/Dashboard.css"
+import { productSectionOne } from '../Components/Product';
+import {productSectionSecond} from "../Components/Product"
 
 const Store = () => {
+
   return (
     <>
       <div className="search_box">
-       <p className="search_title">Search Item</p>
-       <input className="search_input" type="text" placeholder="Apple Watch, Samsung S21, Macbook Pro, ..." />
+        <p className="search_title">Search Item</p>
+        <input className="search_input" type="text" placeholder="Apple Watch, Samsung S21, Macbook Pro, ..." />
+      </div>
+
+      <div className="sale_continer">
+        <div className="sale_section">
+          {
+            productSectionOne.map((product) => (
+              <div className="sale_item" key={product.id}>
+                <img className="sale_item_img" src={product.photo} alt={product.productName} />
+                <h2 className="sale_iten_title">{product.productName}</h2>
+                <p className="sale_item_about">{product.productModel}</p>
+                <div className="sale_item_box">
+                  <span className="sale_item_price">$ {product.price}</span>
+                  <img className="add_to_cart_btn" src={product.addToCartIcon} alt="Add to Cart" />
+                </div>
+              </div>
+            ))
+          }
+        </div>
+
+        <div className='section_second_container'>
+          <div className="sale_section">
+            {
+              productSectionSecond.map((product) => (
+                <div className="sale_item" key={product.id}>
+                  <img className="sale_item_img" src={product.photo} alt={product.productName} />
+                  <h2 className="sale_iten_title">{product.productName}</h2>
+                  <p className="sale_item_about">{product.productModel}</p>
+                  <div className="sale_item_box">
+                    <span className="sale_item_price">$ {product.price}</span>
+                    <img className="add_to_cart_btn" src={product.addToCartIcon} alt="Add to Cart" />
+                  </div>
+                </div>
+              ))
+            }
+          </div>
+        </div>
       </div>
     </>
   )
