0de9382a9446a9cbd8b2d888efa22664b1fde222
nuci <kirianuci@gmail.com>
2023-02-22 19:40:10 +0400

    Add functional search bar for filtering products

--- store-techno/src/Page/Store.jsx
+++ store-techno/src/Page/Store.jsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Style/Dashboard.css"
 import { productSectionOne } from '../Components/Product';
 import {productSectionSecond} from "../Components/Product"
 
 const Store = () => {
+const [searchBar, setSearchBar] = useState("");
 
+const handleSearchInput = (e) => {
+  setSearchBar(e.target.value)
+}
+const filterProductsOne = productSectionOne.filter((product) => {
+  return product.productName.toLowerCase().includes(searchBar.toLowerCase())
+})
+const filterProducSecond = productSectionSecond.filter((product) => {
+  return product.productName.toLowerCase().includes(searchBar.toLowerCase())
+})
   return (
     <>
+    {/* functional search bar that allows users to filter products by name, model, and price.  */}
       <div className="search_box">
         <p className="search_title">Search Item</p>
-        <input className="search_input" type="text" placeholder="Apple Watch, Samsung S21, Macbook Pro, ..." />
+        <input
+         className="search_input"
+         type="text"
+        placeholder="Apple Watch, Samsung S21, Macbook Pro, ..."
+        onChange={handleSearchInput} />
       </div>
 
       <div className="sale_continer">
         <div className="sale_section">
           {
-            productSectionOne.map((product) => (
+            filterProductsOne.map((product) => (
               <div className="sale_item" key={product.id}>
                 <img className="sale_item_img" src={product.photo} alt={product.productName} />
                 <h2 className="sale_iten_title">{product.productName}</h2>
@@ -32,7 +47,7 @@ const Store = () => {
         <div className='section_second_container'>
           <div className="sale_section">
             {
-              productSectionSecond.map((product) => (
+              filterProducSecond.map((product) => (
                 <div className="sale_item" key={product.id}>
                   <img className="sale_item_img" src={product.photo} alt={product.productName} />
                   <h2 className="sale_iten_title">{product.productName}</h2>
